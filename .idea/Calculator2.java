3c8f457b3abf6fd4a3786bfe2545f60319d887f4
Cemaliziypak <cemali.ziypak@hotmail.com>
2021-10-08 19:58:46 +0200

    variablen deklariert

--- .idea/Calculator2.java
+++ .idea/Calculator2.java
@@ -1,5 +1,22 @@
+
+
 public class Calculator2 {
     public static void main(String[] args) {
+        int c = 1 ;
+        double a = 5.5 ;
+        boolean f = true ;
+        char d = 'o' ;
+        String o ="cem" ;
+
+        System.out.println(c);
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(o);
+
+
+
+
 
     }
 }
